fa1543013a1c423f7a8232816f0060478db516bb
evnwttn <evnwttn@gmail.com>
2023-01-10 14:04:44 -0330

    custom app/doc files

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/* <link rel="stylesheet" href="https://use.typekit.net/nnr5nkk.css" /> */}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
