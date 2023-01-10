862ff193e903ca9bd678b433b0ad9117dcb15be5
evnwttn <evnwttn@gmail.com>
2023-01-10 14:02:53 -0330

    Update index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        {/* <meta name="theme-color" content="#272727" /> */}
+        <title>ev</title>
+      </Head>
+      <>Hello</>
+    </>
+  );
+}
