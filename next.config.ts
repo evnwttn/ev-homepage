160df78df2ed7bab7b286a458c6563b7e56f6ed9
evnwttn <evnwttn@gmail.com>
2023-01-10 14:21:47 -0330

    actions config

---
+++ next.config.ts
@@ -0,0 +1,16 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "/";
+let basePath = "";
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY!.replace(/.*?\//, "");
+
+  assetPrefix = `/ev-homepage/`;
+  basePath = `/ev-homepage`;
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+};
