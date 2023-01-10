9969d78544d4c5c4f146291a7dd3b504a39b8cb9
evnwttn <evnwttn@gmail.com>
2023-01-10 15:52:38 -0330

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,7 @@
 import React from "react";
 import { Box } from "@mui/material";
+import { inline } from "../styles";
 
 export const App = () => {
-  return (
-    <Box
-      sx={{
-        color: "#f5dcc0",
-        fontFamily: "forma-djr-display, sans-serif;",
-        fontWeight: "800",
-        fontSize: "4vh",
-      }}
-    >
-      Yo
-    </Box>
-  );
+  return <Box sx={inline.navFontSx}>Yo</Box>;
 };
