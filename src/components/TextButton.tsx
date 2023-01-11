407838f3d6ef36e3109058616c4350cdf3c4fadb
evnwttn <evnwttn@gmail.com>
2023-01-11 14:54:21 -0330

    typing

--- src/components/TextButton.tsx
+++ src/components/TextButton.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Box } from "@mui/material";
 import { inline } from "../styles";
+import { TextButtonProps } from "../types";
 
-export const TextButton = ({ text }) => {
+export const TextButton = ({ text }: TextButtonProps) => {
   return <Box sx={inline.navFontSx}>{text}</Box>;
 };
---
+++ src/types/index.ts
@@ -0,0 +1,1 @@
+export type { TextButtonProps } from "./TextButtonProps";
