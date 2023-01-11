111e4672db611acc7b0c7f4ce5dcca4886626451
evnwttn <evnwttn@gmail.com>
2023-01-11 14:28:34 -0330

    gotcha

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Box } from "@mui/material";
-import { Nav } from "./index";
+import { Nav, Ev } from "./index";
 import { inline } from "../styles";
 
 export const App = () => {
   return (
     <Box sx={inline.appContainer}>
-      <Box sx={inline.appBoxLeft}></Box>
+      <Box sx={inline.appBoxLeft}>
+        <Ev />
+      </Box>
       <Box sx={inline.appBoxRight}>
         <Nav />
       </Box>
--- src/components/Ev.tsx
+++ src/components/Ev.tsx
@@ -3,6 +3,12 @@ import Image from "next/image";
 
 export const Ev = () => {
   return (
-    <Image src="../img/ev.png" alt="A picture of Evan" width="12" height="12" />
+    <Image
+      src="../img/ev.png"
+      alt="A picture of Evan"
+      width="1329"
+      height="2980"
+      style={{ height: "100%", width: "auto" }}
+    />
   );
 };
